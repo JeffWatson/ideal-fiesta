3f095ffca93b09b1135797c134a7fe6127b8e096
Jeff Watson <Jeff.Watson4@gmail.com>
2017-09-23 18:12:22 -0500

    Battle Matrix refactor - player concept

--- src/frontend/components/matrix/data/battleMatrix.js
+++ src/frontend/components/matrix/data/battleMatrix.js
@@ -1,3 +1,4 @@
+import { isEqual, some } from 'lodash';
 import {
   UNITS,
   TERRAIN,
@@ -59,49 +60,79 @@ class BattleMatrix {
     this.matrix = this.matrix.set('grid', this.matrix.get('grid').setIn([row, column, 'actionable'], true));
   }
 
-  calculateMoves({ row, column, move, range, unitProps, currentPlayer }) {
-    this.calculateMove({ row, column, move, range, unitProps, currentPlayer });
+  calculateMoves(firstMove) {
+    const queue = [];
+    this.calculateNextMoves(firstMove, queue);
 
-    const cell = this.getCell({ row, column });
-    if (cell.get('movable') && move >= 0) {
-      this.movement({ row, column, move: move - 1, range, unitProps, currentPlayer });
-    } else if (cell.get('attackable') && move > 0 && range > 1) {
-      this.movement({ row, column, move: 0, range: range - 1, unitProps, currentPlayer });
-    } else if (cell.get('attackable') && !move && range > 1) {
-      this.movement({ row, column, move, range: range - 1, unitProps, currentPlayer });
+    let queueIndex = 0;
+    while (queue.length > queueIndex) {
+      const { row, column, move, range, unitProps, currentPlayer } = queue[queueIndex];
+      this.calculateMove({ row, column, move, range, unitProps, currentPlayer });
+
+      const cell = this.getCell({ row, column });
+      if (cell.get('movable') && move >= 0) {
+        this.calculateNextMoves({ row,
+          column,
+          move: move - 1,
+          range,
+          unitProps,
+          currentPlayer }, queue);
+      } else if (cell.get('attackable') && move > 0 && range > 1) {
+        this.calculateNextMoves({ row,
+          column,
+          move: 0,
+          range: range - 1,
+          unitProps,
+          currentPlayer }, queue);
+      } else if (cell.get('attackable') && !move && range > 1) {
+        this.calculateNextMoves({ row,
+          column,
+          move,
+          range: range - 1,
+          unitProps,
+          currentPlayer }, queue);
+      }
+
+      queueIndex += 1;
     }
   }
 
-  // TODO for performance, use queue?
-  // TODO take in to account selected state. Don't add movable, attackable. Actionable, yes.
-  movement({ row, column, move, range, unitProps, currentPlayer }) {
-    const north = row - 1;
-    const south = row + 1;
-    const west = column - 1;
-    const east = column + 1;
-    if (north >= 0) {
-      this.calculateMoves({ row: north, column, move, range, unitProps, currentPlayer });
+  calculateNextMoves(operation, queue) {
+    const { row, column, move, range, unitProps, currentPlayer } = operation;
+
+    const north = { row: row - 1, column, move, range, unitProps, currentPlayer };
+    const south = { row: row + 1, column, move, range, unitProps, currentPlayer };
+    const west = { row, column: column - 1, move, range, unitProps, currentPlayer };
+    const east = { row, column: column + 1, move, range, unitProps, currentPlayer };
+    if (north.row >= 0 && !this.isInQueue(queue, north)) {
+      queue.push(north);
     }
-    if (west >= 0) {
-      this.calculateMoves({ row, column: west, move, range, unitProps, currentPlayer });
+    if (west.column >= 0 && !this.isInQueue(queue, west)) {
+      queue.push(west);
     }
-    if (south < this.matrix.get('rows')) {
-      this.calculateMoves({ row: south, column, move, range, unitProps, currentPlayer });
+    if (south.row < this.matrix.get('rows') && !this.isInQueue(queue, south)) {
+      queue.push(south);
     }
-    if (east < this.matrix.get('columns')) {
-      this.calculateMoves({ row, column: east, move, range, unitProps, currentPlayer });
+    if (east.column < this.matrix.get('columns') && !this.isInQueue(queue, east)) {
+      queue.push(east);
     }
+  }
+
+  isInQueue(queue, findMe) {
+    return some(queue, object => isEqual(object, findMe));
   }
 
   // TODO unit combination. take into account health. combine like units with sum of health < MAX_HEALTH
+  // TODO terrain modifiers, mountains should be harder to climb, sand too, etc...
   calculateMove({ row, column, move, range, unitProps, currentPlayer }) {
     const cell = this.getCell({ row, column });
     const occupantUnit = cell.get('unit');
+    const isCurrentPlayer = cell.get('player') === currentPlayer;
 
     const attackable = range > 0 && occupantUnit ?
       unitProps.attacks.includes(UNITS[occupantUnit].type) : true;
 
-    if (attackable) {
+    if (attackable && !isCurrentPlayer) {
       this.matrix = this.matrix.set('grid', this.matrix.get('grid').mergeIn([row, column, 'attackable'], attackable));
     }
 
