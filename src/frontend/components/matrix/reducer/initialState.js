061bf0ff1b6255edc5bcbde9a5d41416519c5d4a
Jeff Watson <Jeff.Watson4@gmail.com>
2017-10-14 17:48:45 -0500

    Matrix Refactor - health on units & buildings

--- src/frontend/components/matrix/reducer/initialState.js
+++ src/frontend/components/matrix/reducer/initialState.js
@@ -1,7 +1,6 @@
+// TODO move to only be used in tests!
 import { fromJS } from 'immutable';
 
-
-// TODO need to refactor units... they should have health. Buildings too.
 const initialState = {
   matrix: {
     currentPlayer: 'BLUE',
@@ -9,31 +8,31 @@ const initialState = {
     rows: 5,
     players: ['BLUE', 'GREEN', 'RED', 'YELLOW'],
     grid: [
-      [{ terrain: 'SAND', unit: 'SOLDIER', health: 20, player: 'GREEN' },
+      [{ terrain: 'SAND', unit: { actor: 'SOLDIER', health: 20, player: 'GREEN' } },
         { terrain: 'SAND' },
         { terrain: 'SAND' },
-        { terrain: 'SAND', unit: 'SMALL_TANK', health: 20, player: 'RED' },
+        { terrain: 'SAND', unit: { actor: 'SMALL_TANK', health: 20, player: 'RED' } },
         { terrain: 'SAND' }],
       [{ terrain: 'OCEAN' },
         { terrain: 'OCEAN' },
         { terrain: 'OCEAN' },
         { terrain: 'OCEAN' },
-        { terrain: 'LAND_FACTORY', health: 20, player: 'RED' }],
+        { terrain: 'LAND_FACTORY', building: { health: 20, player: 'RED' } }],
       [{ terrain: 'GRASS' },
         { terrain: 'SAND' },
         { terrain: 'MOUNTAIN' },
-        { terrain: 'GRASS', unit: 'SOLDIER', health: 20, player: 'BLUE' },
+        { terrain: 'GRASS', unit: { actor: 'SOLDIER', health: 20, player: 'BLUE' } },
         { terrain: 'SAND' }],
-      [{ terrain: 'LAND_FACTORY', health: 20, player: 'BLUE' },
+      [{ terrain: 'LAND_FACTORY', building: { health: 20, player: 'BLUE' } },
         { terrain: 'SAND' },
         { terrain: 'SAND' },
-        { terrain: 'GRASS', unit: 'SOLDIER', health: 20, player: 'BLUE' },
+        { terrain: 'GRASS', unit: { actor: 'SOLDIER', health: 20, player: 'BLUE' } },
         { terrain: 'SAND' }],
       [{ terrain: 'SAND' },
         { terrain: 'GRASS' },
-        { terrain: 'SAND', unit: 'SMALL_TANK', health: 20, player: 'BLUE' },
+        { terrain: 'SAND', unit: { actor: 'SMALL_TANK', health: 20, player: 'BLUE' } },
         { terrain: 'SAND' },
-        { terrain: 'SAND', unit: 'SOLDIER', health: 20, player: 'YELLOW' }],
+        { terrain: 'SAND', unit: { actor: 'SOLDIER', health: 20, player: 'YELLOW' } }],
     ],
   },
 };
